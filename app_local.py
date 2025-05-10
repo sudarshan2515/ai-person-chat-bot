06fa7e95c645205511caac5ae0e693adafb18c60
sudarshan2515 <sudarshanbiradar2515@gmail.com>
2025-05-10 14:33:30 +0530

    Update app_local.py

--- app_local.py
+++ app_local.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Personalized AI Assistant", page_icon="🤖", lay
 st.markdown("<h1 style='text-align: center;'>💬 Personalized AI Assistant</h1>", unsafe_allow_html=True)
 
 # --- API Key ---
-api_key = "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"  # move this to st.secrets or env in production
+api_key = st.secrets["CHATGROQ_API_KEY"]  # move this to st.secrets or env in production
 
 # --- Session State ---
 if "chat_history" not in st.session_state:
